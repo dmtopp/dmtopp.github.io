06178b851f00f976c2ddba9762e3dc8a80bd505f
Daniel Topp <dmtopp@gmail.com>
2016-03-07 17:41:44 -0600

    moved more variables to global app object and began drawPauli function

--- octopus/fractalSquares/javascript/app.js
+++ octopus/fractalSquares/javascript/app.js
@@ -1,15 +1,35 @@
-var app = app || {};
+var app = app || {
+  numTentacles: 4,
+  // number of squares in each tentacle
+  limit: 20,
+  tentacleAngles: [],
+  // keeps track of the direction each tentacle should be moving and by how much
+  deltaAngles: [],
+  tentacleBaseWidth: 50,
+  pauli: {
+    xPos: 40,
+    yPos: 658,
+    headRadius: 7,
+    bodyLength: 28,
+    bodyWidth: 10,
+    drawPauli: function
+  },
+  // returns a number between the two parameters
+  randomRange: function(min,max){
+    if (min === 0) return Math.random() * max;
+    else return min + Math.random()*(max-min);
+  },
+  //returns a random RGBA value as a string
+  returnRandomRGBA: function() {
+    return 'rgba('+Math.floor(this.randomRange(0,255))+','+Math.floor(this.randomRange(0,255))+
+    ','+Math.floor(this.randomRange(0,255))+','+Math.random()+')';
+  }
+};
 
-// returns a number between the two parameters
-function randomRange(min,max){
-  if (min === 0) return Math.random() * max;
-  else return min + Math.random()*(max-min);
-}
 
-function returnRandomRGBA() {
-  return 'rgba('+Math.floor(randomRange(0,255))+','+Math.floor(randomRange(0,255))+
-  ','+Math.floor(randomRange(0,255))+','+Math.random()+')';
-}
+
+
+
 
 
 
@@ -29,40 +49,32 @@ function fractalSquares(x,y,sideLength,angle,limit){
   app.context.restore();
 }
 
-function drawTentacles(sideLength,numTentacles,limit){
-  // Set up canvas
-  var x = (app.width/2)-(sideLength/2);
+function drawTentacles(){
+  // Set starting point
+  var x = (app.width/2)-(app.tentacleBaseWidth/2);
   // angle to rotate each new tentacle by
-  var angle = -2*Math.PI/numTentacles;
+  var angle = -2*Math.PI/app.numTentacles;
   // clear the screen
   app.context.clearRect(0,0,app.width,app.width);
   // save our starting point
   app.context.save();
   app.context.translate(x,x);
   // draw all of our tentacles
-  for(i=0; i < numTentacles; i++){
+  for(i=0; i < app.numTentacles; i++){
 
     app.context.rotate(angle);
-    fractalSquares(0,0,sideLength,arrayOfStartingAngles[i],limit);
-    arrayOfStartingAngles[i] += deltaAngles[i];
-    if (arrayOfStartingAngles[i] > Math.PI/6 ||
-        arrayOfStartingAngles[i] < -Math.PI/6) {deltaAngles[i] = -deltaAngles[i]}
+    fractalSquares(0,0,app.tentacleBaseWidth,app.tentacleAngles[i],app.limit);
+    app.tentacleAngles[i] += app.deltaAngles[i];
+    if (app.tentacleAngles[i] > Math.PI/6 ||
+        app.tentacleAngles[i] < -Math.PI/6) {app.deltaAngles[i] = -app.deltaAngles[i]}
   }
   app.context.restore();
 }
 
 
-var numTentacles = 10;
-// array of random starting angles for each of our tentacles
-var arrayOfStartingAngles = [];
-// keeps track of the direction each tentacle should be moving and by how much
-var deltaAngles = [];
-// tentacles start at a random angle betweeen -Pi/2 and Pi/2
-// all tentacles increment at pi/1000
-for (i=0; i < numTentacles; i++){
-  arrayOfStartingAngles.push(randomRange(-Math.PI/12,Math.PI/12));
-  deltaAngles.push(Math.PI/1000);
-}
+
+
+
 
 
 
@@ -71,7 +83,7 @@ for (i=0; i < numTentacles; i++){
 
 
 function animateLoop() {
-  drawTentacles(50,numTentacles,20);
+  drawTentacles();
 }
 
 
@@ -80,5 +92,13 @@ window.onload = function(){
   app.canvas = document.getElementById('canvas');
   app.context = app.canvas.getContext('2d');
   app.width = canvas.width;
+  // initialize tentacle angles
+  // tentacles start at a random angle betweeen -Pi/2 and Pi/2
+  // all tentacles increment at pi/1000
+  for (i=0; i < app.numTentacles; i++){
+    app.tentacleAngles.push(app.randomRange(-Math.PI/12,Math.PI/12));
+    app.deltaAngles.push(Math.PI/1000);
+  }
+
   var intervalID = window.setInterval(animateLoop, 50);
 };
