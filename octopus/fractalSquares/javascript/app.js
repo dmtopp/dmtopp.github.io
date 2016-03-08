7a98b6d9894864ecb51396246869c78553b17f23
Daniel Topp <dmtopp@gmail.com>
2016-03-07 18:41:42 -0600

    drew a prototype pauli and started working on key movement

--- octopus/fractalSquares/javascript/app.js
+++ octopus/fractalSquares/javascript/app.js
@@ -1,4 +1,7 @@
+// contains global variables and utility functions
+
 var app = app || {
+  self: this,
   numTentacles: 4,
   // number of squares in each tentacle
   limit: 20,
@@ -6,14 +9,11 @@ var app = app || {
   // keeps track of the direction each tentacle should be moving and by how much
   deltaAngles: [],
   tentacleBaseWidth: 50,
-  pauli: {
-    xPos: 40,
-    yPos: 658,
-    headRadius: 7,
-    bodyLength: 28,
-    bodyWidth: 10,
-    drawPauli: function
-  },
+  // Variables to keep track of if keys are pressed
+  northDown: false,
+  southDown: false,
+  eastDown: false,
+  westDown: false,
   // returns a number between the two parameters
   randomRange: function(min,max){
     if (min === 0) return Math.random() * max;
@@ -23,10 +23,86 @@ var app = app || {
   returnRandomRGBA: function() {
     return 'rgba('+Math.floor(this.randomRange(0,255))+','+Math.floor(this.randomRange(0,255))+
     ','+Math.floor(this.randomRange(0,255))+','+Math.random()+')';
+  },
+  onKeyDown: function(evt) {
+    console.log('keydown');
+    switch (evt.keyCode) {
+      case 37:
+        app.westDown = true;
+        break;
+      case 38:
+        app.northDown = true;
+        break;
+      case 39:
+        app.eastDown = true;
+        break;
+      case 40:
+        app.southDown = true;
+        break;
+      case 87:
+        app.northDown = true;
+        break;
+      case 65:
+        app.westDown = true;
+        break;
+      case 83:
+        app.southDown = true;
+        break;
+      case 68:
+        app.eastDown = true;
+        break;
+    }
+  },
+  onKeyUp: function(evt) {
+    console.log('keyup')
+    switch (evt.keyCode) {
+      case 37:
+        app.westDown = false;
+        break;
+      case 38:
+        app.northDown = false;
+        break;
+      case 39:
+        app.eastDown = false;
+        break;
+      case 40:
+        app.southDown = false;
+        break;
+      case 87:
+        app.northDown = false;
+        break;
+      case 65:
+        app.westDown = false;
+        break;
+      case 83:
+        app.southDown = false;
+        break;
+      case 68:
+        app.eastDown = false;
+        break;
+      }
   }
+
 };
 
-
+var pauli = {
+  xPos: 40,
+  yPos: 662,
+  headRadius: 7,
+  bodyLength: 28,
+  bodyWidth: 10,
+  dx: 0,
+  dy: 3,
+  drawPauli: function(){
+    app.context.fillStyle = 'orange';
+    app.context.beginPath();
+    app.context.arc(this.xPos+20,this.yPos,10,0,Math.PI*2);
+    app.context.closePath();
+    app.context.fill();
+    app.context.fillStyle = 'black';
+    app.context.fillRect(this.xPos+15,this.yPos+10,10,28);
+  }
+}
 
 
 
@@ -55,7 +131,7 @@ function drawTentacles(){
   // angle to rotate each new tentacle by
   var angle = -2*Math.PI/app.numTentacles;
   // clear the screen
-  app.context.clearRect(0,0,app.width,app.width);
+
   // save our starting point
   app.context.save();
   app.context.translate(x,x);
@@ -83,7 +159,21 @@ function drawTentacles(){
 
 
 function animateLoop() {
+  app.context.clearRect(0,0,app.width,app.width);
   drawTentacles();
+  pauli.drawPauli();
+  if (app.northDown) pauli.dy -= 7;
+  if (app.southDown) pauli.dy += 7;
+  if (app.eastDown) pauli.dy += 7;
+  if (app.westDown) pauli.dy -= 7;
+  // If pauli is going to go off the screen, don't move pauli
+  if (pauli.xPos + pauli.dx > app.width ||
+      pauli.xPos + pauli.dy < 0) pauli.dx = 0;
+  else if (pauli.yPos + pauli.dy > app.width ||
+           pauli.yPos + pauli.dy < 0) pauli.dy = 0;
+  // update Pauli's position
+  pauli.xPos += pauli.dx;
+  pauli.yPos += pauli.dy;
 }
 
 
@@ -101,4 +191,6 @@ window.onload = function(){
   }
 
   var intervalID = window.setInterval(animateLoop, 50);
+  window.addEventListener('keydown',app.onKeyDown,true);
+  window.addEventListener('keyup',app.onKeyUp,true);
 };
